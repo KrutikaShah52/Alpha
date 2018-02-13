ac3fcefa9b4fc18aa3d9ea1d84a7292ae5d01fb2
KrutikaShah52 <krutika.shah@einfochips.com>
2018-02-13 11:20:00 +0530

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209113728) do
+ActiveRecord::Schema.define(version: 20180213051758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20180209113728) do
   create_table "alpha_scaffolds", force: :cascade do |t|
     t.string "title"
     t.string "descreption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
