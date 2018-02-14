43d532ff66df59d5ff9b4b413c53da4b1686a6e7
KrutikaShah52 <krutika.shah@einfochips.com>
2018-02-14 17:45:45 +0530

    need to change for paging

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails', '~> 5.1.4'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 gem 'bcrypt', '~> 3.1.7'
+gem 'will_paginate','3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- app/controllers/alpha_scaffolds_controller.rb
+++ app/controllers/alpha_scaffolds_controller.rb
@@ -4,7 +4,7 @@ class AlphaScaffoldsController < ApplicationController
   # GET /alpha_scaffolds
   # GET /alpha_scaffolds.json
   def index
-    @alpha_scaffolds = AlphaScaffold.all
+    @alpha_scaffolds = AlphaScaffold.paginate(page: params[:page], per_page: 5)
   end
 
   # GET /alpha_scaffolds/1
