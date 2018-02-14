c7fe6dc2676701c52e6b53f637f81bbfbb48aeb2
KrutikaShah52 <krutika.shah@einfochips.com>
2018-02-14 12:54:22 +0530

    edit and update is done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
     def new
         @user = User.new
     end
@@ -14,6 +15,19 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to alpha_scaffolds_path
+        else
+            render 'edit'
+        end
+    end
+    
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
